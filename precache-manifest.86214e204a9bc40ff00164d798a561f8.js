a15136219557fbb6a0ae4b4a8f4850f151cb5872
Sharath <314sharath@gmail.com>
2021-08-13 03:54:32 +1000

    Updates

---
+++ precache-manifest.86214e204a9bc40ff00164d798a561f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95120365811c6abb18055a9abe5109ca",
+    "url": "/TFLTest/index.html"
+  },
+  {
+    "revision": "cde27c32b795ac0b1f43",
+    "url": "/TFLTest/static/css/main.24904614.chunk.css"
+  },
+  {
+    "revision": "4c7fa34c774fc6f4e8e9",
+    "url": "/TFLTest/static/js/2.7b9eddc1.chunk.js"
+  },
+  {
+    "revision": "cde27c32b795ac0b1f43",
+    "url": "/TFLTest/static/js/main.d03c5bee.chunk.js"
+  },
+  {
+    "revision": "6dc3e01974478623fb1b",
+    "url": "/TFLTest/static/js/runtime~main.91e57101.js"
+  }
+]);
